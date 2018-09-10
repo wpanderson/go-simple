7657c809b7f0d247021a386baa4acc90b1f8f298
wpanderson <weston.anderson@siliconmechanics.com>
2018-09-10 15:34:53 -0700

    Initial commit for outline for project structure

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+// Author: Wes Anderson
+// Descipriont:
+// Start of go-simple a tftp server written in golang. Application starts a simple tftp server which offers in memory tftp servicies.
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+
+	// TODO implement the in-memory tftp server
+}
